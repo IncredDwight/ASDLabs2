59005c05c2ccb533cd25041809d8861038dd102f
Yaroslav_Valchyshen <51487067+IncredDwight@users.noreply.github.com>
2022-02-17 23:25:53 +0200

    Add files via upload

---
+++ Lab1/ASDLab1/ASDLab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SortingAlgorithms;
+using ArrayOperations;
+
+namespace ASDLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort bubbleSort = new BubbleSort("Bubble sort");
+            CombSort combSort = new CombSort("Comb sort");
+
+            int[] array = ArrayHelper.GenerateReversedSequence(1, 50000);
+            ArrayHelper.Display(array);
+            combSort.Sort(array);
+            ArrayHelper.Display(array, "Sorted: ");
+            combSort.DisplaySortResult();
+        }
+    }
+}
